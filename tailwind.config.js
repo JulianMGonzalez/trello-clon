500a8d250bcf61976c390960189f050c11775843
JulianMGonzalez <monterogjulian@gmail.com>
2023-04-19 22:39:59 -0500

    add styles colors customs and style cdk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      success: colors.green,
+      primary: colors.blue,
+      ...colors
+    },
     extend: {},
   },
   plugins: [
